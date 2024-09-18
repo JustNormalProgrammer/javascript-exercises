12090ad31a254eeb891d28a8b96394159b6a24cc
JustNormalProgrammer <testkonto1290@gmail.com>
2024-09-18 13:18:05 +0200

    Add solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    let cleanedString = string.toLowerCase().split('').filter((value) => {
+        return alphanumerical.includes(value);
+    }).join('');
+    let stringReversed = cleanedString.split('').reverse().join('');
+    return cleanedString === stringReversed;
 
 };
 
