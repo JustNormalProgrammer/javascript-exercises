f231c94343b3b9428d726415e8b5dfa1a43a6de2
JustNormalProgrammer <testkonto1290@gmail.com>
2024-09-18 14:01:31 +0200

    Add solution to ex. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    let year = new Date().getFullYear();
+    return arr.reduce((acc, value) => {
+        let ageCurrent = null;
+        if(value.yearOfDeath === undefined){
+            ageCurrent = year - value.yearOfBirth;
+        } else{
+            ageCurrent = value.yearOfDeath - value.yearOfBirth;
+        }
+        if(acc.yearOfDeath === undefined){
+            ageOldest = year - acc.yearOfBirth;
+        } else{
+            ageOldest = acc.yearOfDeath - acc.yearOfBirth;
+        }
+        return ageOldest > ageCurrent ? acc : value;
+    })
+    // const getAge = function (birth, death) {
+    //     if (!death) {
+    //       death = new Date().getFullYear();
+    //     }
+    //     return death - birth;
+    //   };
+      
+    //   const findTheOldest = function (people) {
+    //     return people.reduce((oldest, currentPerson) => {
+    //       const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    //       const currentAge = getAge(
+    //         currentPerson.yearOfBirth,
+    //         currentPerson.yearOfDeath
+    //       );
+    //       return oldestAge < currentAge ? currentPerson : oldest;
+    //     });
+    //   };
+      
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
