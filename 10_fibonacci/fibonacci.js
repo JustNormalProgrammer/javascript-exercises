ae017dcb124fd8ecb714a692cdf1a495d4b343de
JustNormalProgrammer <testkonto1290@gmail.com>
2024-09-18 13:30:40 +0200

    Add solution to ex.10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let count;
+    if (typeof num !== 'number') {
+        count = parseInt(num)
+    } else {
+        count = num
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let arr = [0,1];
+    for(let i = 2; i<=count;i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[count];
 };
 
 // Do not edit below this line
