fd9fc6fd58d82017e2f0c8c9df0d8479829784b3
JustNormalProgrammer <testkonto1290@gmail.com>
2024-09-18 13:38:02 +0200

    Add solution to ex.11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    // return arr.reduce((acc, value) => {
+    //     acc.push(value.title)
+    //     return acc;
+    // }
+    // ,[]);
+    return arr.map(value => value.title);
 };
 
 // Do not edit below this line
